afc04f37868efd5bab770e59059a1825525b59d2
lkups <l.kups@itm.biz.pl>
2017-04-07 11:08:40 +0200

    temp solution for exercise

--- 10 (exercise)/Exercise 4/app.js
+++ 10 (exercise)/Exercise 4/app.js
@@ -1,10 +1,40 @@
 new Vue({
   el: '#exercise',
   data: {
+    firstClass: {backgroundColor: 'pink'},
+    secondClass: {display: 'block', margin: '0 auto', textAlign: 'center'},
+    customClass: '',
+    plainStyleList: '',
+  },
+  computed: {
+    styleList: function(){
+      var arr = this.plainStyleList.split(';');
+      var resultArr = [];
 
+      for(var item in arr){
+        var styles = {};
+        var splited = arr[item].split(':');
+
+        if(splited.length === 2){
+          styles[splited[0]] = splited[1]
+          resultArr.push(styles);
+        }
+      }
+      console.log(resultArr);
+      return resultArr;
+    }
   },
   methods: {
     startEffect: function() {
+      var div = document.getElementById('effect'); 
+      
+      if(div.classList.contains('highlight')){
+        div.className = 'shrink';
+      }else{
+        div.className = 'highlight';
+      }
+    },
+    startProgress: function(){
     
     }
   }
