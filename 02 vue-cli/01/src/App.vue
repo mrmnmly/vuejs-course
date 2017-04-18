9bdceed2c8e37a65eb41748c98fb3b1bde1c1027
mrmnmly <lukaszkups@gmail.com>
2017-04-18 23:40:58 +0200

    setup new vuejs project using vue-cli

---
+++ 02 vue-cli/01/src/App.vue
@@ -0,0 +1,11 @@
+<template><!-- template is always required - the rest can be deleted and it will be still a valid single file vue component -->
+  <h1>Hello world!</h1>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
