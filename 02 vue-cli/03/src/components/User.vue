52d5c9695e243a86a1b7f3fda967109a8f948171
mrmnmly <lukaszkups@gmail.com>
2017-04-20 11:59:05 +0200

    learnt about passing data from parent to child thanks to props

---
+++ 02 vue-cli/03/src/components/User.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :name="name"></app-user-detail> <!-- :name must equal the property in string that we defined in UserDetail.vue file in an props array (props: ['name'] )-->
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+      data: function(){
+        return {
+          name: 'Max'
+        };
+      },
+      methods: {
+        changeName(){
+          this.name = 'Anna';
+        }
+      },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ 02 vue-cli/03/src/components/UserDetail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ name }}</p>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: ['name'] // props attribute are the properties from parent components - in this case - User.vue component data called 'name'
+  }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
