84d383ca3274748e704621b56b76031b96e7a65d
mrmnmly <lukaszkups@gmail.com>
2017-04-21 23:34:58 +0200

    learnt about slot

---
+++ 02 vue-cli/05/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+              <app-quote>
+                <h2>{{ quoteTitle }}</h2>
+                <p>A wonderful Quote!</p>
+              </app-quote>             
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue';
+
+    export default {
+      data: function(){
+        return {
+          quoteTitle: 'The Quote'
+        };
+      },
+      components: {
+        appQuote: Quote
+      }        
+    }
+</script>
+
+<style>
+</style>
---
+++ 02 vue-cli/05/src/components/Quote.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <slot></slot>  <!-- <slot></slot> is a reserved, built-in type of component in Vue.js that takes HTML content of parent component (in this case, from App.vue file the insides of <app-quote> --> 
+  </div>
+</template>
+
+<script>
+  export default {
+   
+  }
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+  }
+  h2 {  /* this local styling is applied to <slot></slot> contents - not the parent! */
+    color: red;
+  }
+  .title {
+    font-style: italic;
+  }
+</style>
