449b4a8968a7bbf072a3c06da7ba0ce8dc9e81cb
mrmnmly <lukaszkups@gmail.com>
2017-04-23 12:50:29 +0200

    learnt about dynamic components and loaded exercise code

--- 02 vue-cli/05/src/App.vue
+++ 02 vue-cli/05/src/App.vue
@@ -2,12 +2,24 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-              <app-quote>
+              <button @click="selectedComponent = 'appQuote'">Quote</button>
+              <button @click="selectedComponent = 'appAuthor'">Author</button>
+              <button @click="selectedComponent = 'appNew'">New</button>
+              <hr>
+              <p>Selected Component: {{ selectedComponent }}</p>
+              <component :is="selectedComponent"> <!-- reserved word for Vue - a dynamic component directive when switching between components they're destoyed and recreated brand new -->
+                <p>Default Content</p>
+              </component>
+              <hr>
+              <keep-alive> <!-- this is reserved word for Vue - it keeps alive components when we switch between them in <component :is .. > directive - yay! :D -->
+                <component :is="selectedComponent"></component>
+              </keep-alive>
+              <!-- <app-quote>
                 <h2 slot="title">{{ quoteTitle }}</h2>
                 <p slot="content">A wonderful Quote!</p>
                 <hr>
                 <p>This will be contents of unnamed slot :)</p>
-              </app-quote>             
+              </app-quote> -->
             </div>
         </div>
     </div>
@@ -15,15 +27,20 @@
 
 <script>
   import Quote from './components/Quote.vue';
+  import Author from './components/Author.vue';
+  import New from './components/New.vue';
 
     export default {
       data: function(){
         return {
-          quoteTitle: 'The Quote'
+          quoteTitle: 'The Quote',
+          selectedComponent: 'appQuote'
         };
       },
       components: {
-        appQuote: Quote
+        appQuote: Quote,
+        appAuthor: Author,
+        appNew: New
       }        
     }
 </script>
---
+++ 02 vue-cli/05/src/components/New.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h3>New Quote</h3>
+    <button @click="counter++">Increase!</button>
+    <p>Counter: {{ counter }}</p>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function(){
+      return {
+        counter: 0
+      };
+    },
+    destroyed(){
+      console.log('Destroyed!');
+    },
+    deactivated(){
+      console.log('Switched to other component!');
+    },
+    activated(){
+      console.log('Switched to New component!');
+    }
+  }
+</script>
+
+<style></style>
