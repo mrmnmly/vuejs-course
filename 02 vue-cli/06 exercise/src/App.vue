d4007eb42e0016e2f10e73be28ff4ac97da7a69a
mrmnmly <lukaszkups@gmail.com>
2017-04-23 13:48:26 +0200

    solved exercise

---
+++ 02 vue-cli/06 exercise/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="selectedColor = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedColor = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedColor = 'appRed'">Load Red Template</button>
+                <hr>
+                <component :is="selectedColor">
+                  <p>Default content</p>
+                </component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        data: function(){
+          return {
+            selectedColor: 'appBlue'
+          };
+        }
+    }
+</script>
+
+<style>
+</style>
