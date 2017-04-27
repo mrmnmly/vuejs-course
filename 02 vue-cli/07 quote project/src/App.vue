cf3a66f698279d546b26251950bde55dafecab84
mrmnmly <lukaszkups@gmail.com>
2017-04-27 23:57:18 +0200

    project wip commit

---
+++ 02 vue-cli/07 quote project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+      <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <div class="alert alert-inf"></div>
+        </div>  
+      </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+
+    export default {
+      data: function(){
+        return {
+          quotes: ['Just a Quote to see something'],
+          maxQuotes: 10
+        };
+      },
+      methods: {
+        newQuote(quote){
+           this.quotes.push(quote);  
+        }
+      },
+      components: {
+        appQuoteGrid: QuoteGrid,
+        appNewQuote: NewQuote
+      }
+    }
+</script>
+
+<style>
+</style>
