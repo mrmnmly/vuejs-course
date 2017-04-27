0bc73a5dd099619251975f6362d0bf6b750d3ead
mrmnmly <lukaszkups@gmail.com>
2017-04-28 01:34:05 +0200

    finished project

--- 02 vue-cli/07 quote project/src/App.vue
+++ 02 vue-cli/07 quote project/src/App.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="container">
+      <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
       <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-      <app-quote-grid :quotes="quotes"></app-quote-grid>
+      <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
       <div class="row">
         <div class="col-sm-12 text-center">
-          <div class="alert alert-inf"></div>
+          <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
         </div>  
       </div>
     </div>
@@ -13,6 +14,7 @@
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
     import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
 
     export default {
       data: function(){
@@ -23,12 +25,19 @@
       },
       methods: {
         newQuote(quote){
-           this.quotes.push(quote);  
+          if(this.quotes.length >= this.maxQuotes){
+            return alert('Please delete Quotes first!');
+          }
+          this.quotes.push(quote);  
+        },
+        deleteQuote(index){
+          this.quotes.splice(index, 1);
         }
       },
       components: {
         appQuoteGrid: QuoteGrid,
-        appNewQuote: NewQuote
+        appNewQuote: NewQuote,
+        appHeader: Header
       }
     }
 </script>
--- 02 vue-cli/07 quote project/src/components/QuoteGrid.vue
+++ 02 vue-cli/07 quote project/src/components/QuoteGrid.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <app-quote v-for="quote in quotes">
+    <app-quote v-for="(quote, index) in quotes" @click.native="deleteQuote(index)"><!-- @click.native modifier proxies click event to slot'ed {{ quote }} element. We have to pass index to deleteQuote to know which element to delete from quotes array -->
       {{ quote }}
     </app-quote> 
   </div>
@@ -12,6 +12,11 @@
     props: ['quotes'],
     components: {
       appQuote: Quote
+    },
+    methods: {
+      deleteQuote(index) {
+        this.$emit('quoteDeleted', index);
+      }
     }
   }
 </script>
