7694b012fdf5b51adae6475ce3ff36c2d76fa116
mrmnmly <lukaszkups@gmail.com>
2017-05-05 11:39:21 +0200

    learnt about use cases of filters and computed properties

--- 02 vue-cli/12/src/App.vue
+++ 02 vue-cli/12/src/App.vue
@@ -6,6 +6,11 @@
               <p>{{ text | toUppercase }}</p>
               <p>{{ text | to-lowercase }}</p>
               <p>{{ text | toUppercase | to-lowercase }}</p>
+              <hr>
+              <input v-model="filterText">
+              <ul><!--  we shouldn't use filters here, because they're recalculated on each render - no matter if something changed or not - it's better to use computed property here   -->
+                <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+              </ul>
             </div>
         </div>
     </div>
@@ -15,13 +20,22 @@
     export default {
       data() {
         return {
-          text: 'Hello there!' 
+          text: 'Hello there!',
+          fruits: ['apple', 'banana', 'mango', 'melon'],
+          filterText: ''
         };
       },
       filters: { // filters TRANSFORMS the way of data looks in the template/output - it doesn't transform data ITSELF! !
         toUppercase(value) { // filters always has a value to filter as a first argument!
           return value.toUpperCase(); 
         }       
+      },
+      computed: {
+        filteredFruits() {
+          return this.fruits.filter((element) => {
+            return element.match(this.filterText);
+          });
+        } 
       }
     }
 </script>
