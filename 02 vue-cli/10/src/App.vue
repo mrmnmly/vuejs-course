01dd2421ffbcbe9e5ac6724a9111f1fdb2f52956
mrmnmly <lukaszkups@gmail.com>
2017-05-05 11:04:58 +0200

    learnt about local directives and passing objects as a binding values

--- 02 vue-cli/10/src/App.vue
+++ 02 vue-cli/10/src/App.vue
@@ -14,6 +14,7 @@
                 <p v-highlight:background="'red'">Color this</p>
                 <p v-highlight="'red'">Color this</p>
                 <p v-highlight:background.delayed="'red'">Color this</p>
+                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 1000}">Color this too</p>
             </div>
         </div>
     </div>
@@ -21,6 +22,41 @@
 
 <script>
     export default {
+      directives: { //  local directives
+        'local-highlight': {
+          bind(el, binding, vnode) {
+            let delay = 0;
+
+            if(binding.modifiers['delayed']){
+              delay = 3000;  
+            }
+            if(binding.modifiers['blink']){
+              let mainColor = binding.value.mainColor;
+              let secondColor = binding.value.secondColor;
+              let currentColor = mainColor;
+              
+              setTimeout(() => {
+                setInterval(() => {
+                  currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+                  if(binding.arg == 'background'){
+                    el.style.backgroundColor = currentColor;
+                  }else{
+                    el.style.color = currentColor;
+                  }
+                }, binding.value.delay); 
+              }, delay);
+            }else{
+              setTimeout(() => {
+                if(binding.arg == 'background'){
+                  el.style.backgroundColor = binding.value.mainColor;
+                }else{
+                  el.style.color = binding.value.mainColor;
+                }
+              }, delay);
+            }
+          }
+        }
+      }
     }
 </script>
 
