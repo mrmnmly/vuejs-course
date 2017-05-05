f235c487ffd94713ba45ee8b484b57ba526a9aad
mrmnmly <lukaszkups@gmail.com>
2017-05-05 11:58:53 +0200

    learnt about global mixins

--- 02 vue-cli/12/src/main.js
+++ 02 vue-cli/12/src/main.js
@@ -5,7 +5,17 @@ Vue.filter('to-lowercase', function(value){ // global filter
   return value.toLowerCase();
 });
 
+// global mixin below - you will be rarely want to do this - mostly when creating a plugin, not into production app itself
+// it will be mixed in to every Vue's instances AUTOMATICALLY !!
+Vue.mixin({
+  created() {
+    console.log('global mixin!');
+  }
+});
+
+// global mixin lands also here ;)
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+// global mixins runs first, then local mixins, and component code last - has the last word ;)
