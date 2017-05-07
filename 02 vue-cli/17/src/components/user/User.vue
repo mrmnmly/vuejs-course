bd0985fcad907883d1179ba182bc18fbfb7eed28
mrmnmly <lukaszkups@gmail.com>
2017-05-07 12:53:14 +0200

    learnt about nested routes

--- 02 vue-cli/17/src/components/user/User.vue
+++ 02 vue-cli/17/src/components/user/User.vue
@@ -2,13 +2,15 @@
   <div>
     <h1>The User Page</h1>
     <hr>
-    <p>Loaded ID: {{ id }}</p>
+    <!--<p>Loaded ID: {{ id }}</p>-->
     <button class="btn btn-primary" @click="navigateToHome">Go to Home</button>
+    <hr>
+    <router-view></router-view>
   </div>
 </template>
 <script>
   export default {
-    data() {
+    /*(data() {
       return {
         id: this.$route.params.id // access route params example
       }
@@ -17,7 +19,7 @@
       '$route'(to, from) { // watch for route changes to update id when switching between same component but different ids
         this.id = to.params.id;
       }
-    },
+    },*/
     methods: {
       navigateToHome() {
         this.$router.push('/'); // js-based navigation example
