eb5f13a94c43ecdd0c0300a4c8d6ba0741cfec55
mrmnmly <lukaszkups@gmail.com>
2017-05-07 11:56:56 +0200

    added basic router

--- 02 vue-cli/17/src/App.vue
+++ 02 vue-cli/17/src/App.vue
@@ -3,6 +3,8 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
+                <hr>
+                <router-view></router-view><!-- built-in component, which allows to render here routed part of the app -->
             </div>
         </div>
     </div>
--- 02 vue-cli/17/src/main.js
+++ 02 vue-cli/17/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import App from './App.vue'
+import { routes } from './routes';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes // ES6 key-value short syntax
+});
 
 new Vue({
   el: '#app',
+  router, // again, ES6 short key-value syntax (router: router)
   render: h => h(App)
 })
