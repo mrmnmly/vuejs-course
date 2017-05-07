8615819e485b98f4b45179eef3a363735cb2d79f
mrmnmly <lukaszkups@gmail.com>
2017-05-07 12:36:11 +0200

    added dynamic route example

--- 02 vue-cli/17/src/components/user/User.vue
+++ 02 vue-cli/17/src/components/user/User.vue
@@ -1,4 +1,22 @@
 <template>
+  <div>
     <h1>The User Page</h1>
-
+    <hr>
+    <p>Loaded ID: {{ id }}</p>
+    <button class="btn btn-primary" @click="navigateToHome">Go to Home</button>
+  </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        id: this.$route.params.id // access route params example
+      }
+    },
+    methods: {
+      navigateToHome() {
+        this.$router.push('/'); // js-based navigation example
+      }
+    }
+  }
+</script>
--- 02 vue-cli/17/src/routes.js
+++ 02 vue-cli/17/src/routes.js
@@ -3,5 +3,6 @@ import Home from './components/Home.vue';
 
 export const routes = [
   { path: '', component: Home },
-  { path: '/user', component: User }
+ //  { path: '/user', component: User }
+  { path: '/user/:id', component: User }
 ];
