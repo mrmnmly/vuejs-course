b475d7a6a2b0f97f5d4d4e3e0a608c67fba1cb34
mrmnmly <lukaszkups@gmail.com>
2017-05-08 18:25:23 +0200

    learnt about scrolling

--- 02 vue-cli/17/src/components/user/UserDetail.vue
+++ 02 vue-cli/17/src/components/user/UserDetail.vue
@@ -4,8 +4,27 @@
     <p>User loaded has ID: {{ $route.params.id }}</p>
     <router-link 
       tag="button"
-      :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100 } }"
+      :to="link"
       class="btn btn-primary">Edit User</router-link>
     <!--  :to="'/user/' + $route.params.id + '/edit'" - previous example of dynamic url - before using a route name -->
   </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        link: {
+          name: 'userEdit', 
+          params: { 
+            id: this.$route.params.id 
+          }, 
+          query: { 
+            locale: 'en', 
+            q: 100 
+          },
+          hash: '#data' // for now, page won't scroll to the bottom, even if hash is in the url
+        }
+      }; 
+    }
+  }
+</script>
--- 02 vue-cli/17/src/main.js
+++ 02 vue-cli/17/src/main.js
@@ -7,7 +7,16 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes, // ES6 key-value short syntax
-  mode: 'history' //to use urls without hashtag we have to config our server to server always index.html to all url cases!!!
+  mode: 'history', //to use urls without hashtag we have to config our server to server always index.html to all url cases!!!
+  scrollBehavior(to, from, savedPosition) { // handles scrolling to local anchor when hash is present in the url path
+    if(savedPosition) { // if user goes back to page where he scrolled somewhere, then we return the value of that scroll so he will be on the same place again
+      return savedPosition;
+    }
+    if(to.hash) {
+      return { selector: to.hash }; // if hash (local anchor) is available in path, then scroll to it, otherwise stay on top
+    }
+    //return {x: 0, y: 700}; 
+  }
 });
 
 new Vue({
