dd70353a513cd5e8e869d5800963d1456c3db1e1
mrmnmly <lukaszkups@gmail.com>
2017-05-08 09:53:46 +0200

    learnt about multiple router-views with names

--- 02 vue-cli/17/src/App.vue
+++ 02 vue-cli/17/src/App.vue
@@ -4,8 +4,9 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
                 <hr>
-                <app-header></app-header>
-                <router-view></router-view><!-- built-in component, which allows to render here routed part of the app -->
+                <router-view name="header-top"></router-view><!-- built-in component, which allows to render here routed part of the app -->
+                <router-view></router-view><!-- default router view -->
+                <router-view name="header-bottom"></router-view>
             </div>
         </div>
     </div>
--- 02 vue-cli/17/src/routes.js
+++ 02 vue-cli/17/src/routes.js
@@ -3,12 +3,19 @@ import UserStart from './components/user/UserStart.vue';
 import UserDetail from './components/user/UserDetail.vue';
 import UserEdit from './components/user/UserEdit.vue';
 import Home from './components/Home.vue';
+import Header from './components/Header.vue';
 
 export const routes = [
-  { path: '', component: Home, name: 'home' },
+  { path: '', name: 'home', components: {
+    default: Home,
+    'header-top': Header
+  } },
  //  { path: '/user', component: User }
 //  { path: '/user/:id', component: User }
-   { path: '/user', component: User, children: [
+   { path: '/user', components: {
+    default: User,
+    'header-bottom': Header
+   }, children: [
     { path: '', component: UserStart },
     { path: ':id', component: UserDetail },
     { path: ':id/edit', component: UserEdit, name: 'userEdit' }
