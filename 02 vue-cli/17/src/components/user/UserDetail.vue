058b2329ac14889c3ad918152473241e4dabe8e9
mrmnmly <lukaszkups@gmail.com>
2017-05-08 09:33:05 +0200

    learnt about query params

--- 02 vue-cli/17/src/components/user/UserDetail.vue
+++ 02 vue-cli/17/src/components/user/UserDetail.vue
@@ -4,7 +4,7 @@
     <p>User loaded has ID: {{ $route.params.id }}</p>
     <router-link 
       tag="button"
-      :to="{ name: 'userEdit', params: { id: $route.params.id } }"
+      :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100 } }"
       class="btn btn-primary">Edit User</router-link>
     <!--  :to="'/user/' + $route.params.id + '/edit'" - previous example of dynamic url - before using a route name -->
   </div>
