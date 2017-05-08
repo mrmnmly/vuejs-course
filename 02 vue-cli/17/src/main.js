fca5e566c4800d9a868debccf1dae2fb49bfc3e2
mrmnmly <lukaszkups@gmail.com>
2017-05-08 18:32:33 +0200

    set global before each route function

--- 02 vue-cli/17/src/main.js
+++ 02 vue-cli/17/src/main.js
@@ -9,7 +9,7 @@ const router = new VueRouter({
   routes, // ES6 key-value short syntax
   mode: 'history', //to use urls without hashtag we have to config our server to server always index.html to all url cases!!!
   scrollBehavior(to, from, savedPosition) { // handles scrolling to local anchor when hash is present in the url path
-    if(savedPosition) { // if user goes back to page where he scrolled somewhere, then we return the value of that scroll so he will be on the same place again
+    if(savedPosition) { // if user goes back to page where he scrolled somewhere, then we return the value of that scroll so he will be on the same place againe
       return savedPosition;
     }
     if(to.hash) {
@@ -17,6 +17,13 @@ const router = new VueRouter({
     }
     //return {x: 0, y: 700}; 
   }
+});
+
+router.beforeEach((to, from, next) => { // before guard - executed before every route change
+  console.log('global before each');
+  //next('/'); // you can use next() to execute custom reroute (even use object with route name etc.)
+  //next(false); // this will stop current request immediately
+  next();
 });
 
 new Vue({
