984dbad225b08ada967c9f639523c2381adf676a
mrmnmly <lukaszkups@gmail.com>
2017-05-08 18:08:44 +0200

    added route transition animations

--- 02 vue-cli/17/src/App.vue
+++ 02 vue-cli/17/src/App.vue
@@ -5,7 +5,9 @@
                 <h1>Routing</h1>
                 <hr>
                 <router-view name="header-top"></router-view><!-- built-in component, which allows to render here routed part of the app -->
-                <router-view></router-view><!-- default router view -->
+                <transition name="slide" mode="out-in">
+                  <router-view></router-view><!-- default router view -->
+                </transition>
                 <router-view name="header-bottom"></router-view>
             </div>
         </div>
@@ -23,4 +25,34 @@
 </script>
 
 <style>
+  .slide-leave-active {
+    transition: opacity 1s ease;
+    opacity: 0;
+    animation: slide-out 1s ease-out forwards;
+  }
+  .slide-leave {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  .slide-enter-active {
+     animation: slide-in 1s ease-out forwards;
+     opacity: 1;
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(-30px);
+    }
+  }
+  @keyframes slide-in {
+    from {
+      transform: translateY(-30px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
 </style>
