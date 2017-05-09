daef10a59fe52fcccd37ac69f35495da706c305a
mrmnmly <lukaszkups@gmail.com>
2017-05-09 23:50:56 +0200

    temp commit

--- 02 vue-cli/17/src/routes.js
+++ 02 vue-cli/17/src/routes.js
@@ -1,9 +1,12 @@
-import User from './components/user/User.vue';
-import UserStart from './components/user/UserStart.vue';
-import UserDetail from './components/user/UserDetail.vue';
-import UserEdit from './components/user/UserEdit.vue';
 import Home from './components/Home.vue';
 import Header from './components/Header.vue';
+
+// webpack's dynamic loading necessary code (splits bundle into multiple bundles)
+const User = resolve => {
+  require.ensure(['./components/user/User.vue'], () => {
+    resolve(require('./components/user/User.vue'));
+  });
+};
 
 export const routes = [
   { path: '', name: 'home', components: {
