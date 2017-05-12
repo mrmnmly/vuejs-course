aa8116caeba9d8f3d5177eade6bf356aded9c020
mrmnmly <lukaszkups@gmail.com>
2017-05-12 12:57:06 +0200

    finished lesson about lazy loading bundles

--- 02 vue-cli/17/src/routes.js
+++ 02 vue-cli/17/src/routes.js
@@ -5,22 +5,22 @@ import Header from './components/Header.vue';
 const User = resolve => {
   require.ensure(['./components/user/User.vue'], () => {
     resolve(require('./components/user/User.vue'));
-  });
+  }, 'userBundle'); // optional argument, if it won't be passed then every require below would be loaded separately when necessary. With a name, it creates a grup, which contains bundle for all lazy loaded modules having that as a second argument in definition
 };
 const UserStart = resolve => {
   require.ensure(['./components/user/UserStart.vue'], () => {
     resolve(require('./components/user/UserStart.vue'));
-  });
+  }, 'userBundle');
 }
 const UserEdit = resolve => {
   require.ensure(['./components/user/UserEdit.vue'], () => {
     resolve(require('./components/user/UserEdit.vue'));
-  });
+  }, 'userBundle');
 }
 const UserDetail = resolve => {
   require.ensure(['./components/user/UserDetail.vue'], () => {
     resolve(require('./components/user/UserDetail.vue'));
-  });
+  }, 'userBundle');
 }
 
 export const routes = [
